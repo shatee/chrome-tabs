cc6bc33372b57233645a189749cb33bb7033ae75
SHATE <5866770+shatee@users.noreply.github.com>
2023-09-09 22:16:03 +0900

    prettierrc

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,11 +1,11 @@
-import { defineManifest } from "@crxjs/vite-plugin";
+import { defineManifest } from '@crxjs/vite-plugin';
 
 export const manifest = defineManifest({
   manifest_version: 3,
-  name: "chrome-tabs",
-  description: "chrome extension",
-  version: "0.0.1",
+  name: 'chrome-tabs',
+  description: 'chrome extension',
+  version: '0.0.1',
   action: {
-    default_popup: "index.html",
+    default_popup: 'index.html',
   },
 });
