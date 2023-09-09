16c6ac7f19b4fcfc6bf37e66b873f0e88f7fedf8
SHATE <5866770+shatee@users.noreply.github.com>
2023-09-09 23:54:12 +0900

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React, { useCallback, useEffect, useRef, useTransition } from 'react';
 import './App.css';
 import { searchWordAtom } from './atoms/searchWordAtom';
 import { browserHistoryAtom } from './atoms/browserHistoryAtom';
+import { browserTabAtom } from './atoms/browserTabAtom';
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [searchWord, setSearchWord] = useAtom(searchWordAtom);
 
+  const [tabs] = useAtom(browserTabAtom);
   const [histories] = useAtom(browserHistoryAtom);
 
   const [, startTransition] = useTransition();
@@ -29,6 +31,12 @@ function App() {
       <div>
         <input type="text" onInput={onSearchInputChange} ref={inputRef} />
         <ul>
+          {tabs.map((tab) => (
+            <li>
+              <p>{tab.title}</p>
+              <p>{tab.url}</p>
+            </li>
+          ))}
           {histories.map((history) => (
             <li>
               <p>{history.title}</p>
---
+++ src/atoms/browserTabAtom.ts
@@ -0,0 +1,17 @@
+import { atom } from 'jotai';
+import { searchWordAtom } from './searchWordAtom';
+
+const LIMIT = 10;
+
+export const browserTabAtom = atom(async (get) => {
+  const searchWord = get(searchWordAtom);
+
+  const tabs = await chrome.tabs.query({
+    lastFocusedWindow: true,
+  });
+  return tabs
+    .filter((tab) => {
+      return tab.url?.includes(searchWord) || tab.title?.includes(searchWord);
+    })
+    .slice(0, LIMIT);
+});
